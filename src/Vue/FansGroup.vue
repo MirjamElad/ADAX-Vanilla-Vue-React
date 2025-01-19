9635e0a9c7f854d0039f2a5771d2a687c90f8cfc
MirjamElad <mirjamelad@gmail.com>
2025-01-19 23:13:51 +0100

    better support for vue typing

--- src/Vue/FansGroup.vue
+++ src/Vue/FansGroup.vue
@@ -6,14 +6,14 @@ import { useSync, trigger } from 'adax-vue';
 const props = defineProps(['name']);
 
 const data = useSync(getMood, () => ({ name: props.name }));
-console.log('data?.mood:', data?.mood);
+console.log('data?.mood:', data.mood);
 const vote = () => trigger(voteFor, { name: props.name });
 //@ts-ignore
 window.vote = vote;
 </script>
 <template>
   <div class="bg-white rounded-xl shadow-lg mx-6 px-6 py-4 my-2">
-    <h1>{{ name }} FANS : {{ data?.mood }}</h1>
+    <h1>{{ name }} FANS: {{ data?.mood }}</h1>
     <button
       @click="vote"
       class="bg-gray-400 mt-4 px-4 py-2 rounded-lg border border-slate-500 shadow-2xl"
